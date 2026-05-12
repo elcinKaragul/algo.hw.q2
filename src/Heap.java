ffa2b22c86f55e8e970066fec84c18428390f5f7
Kayra <kayra.ari@tedu.edu.tr>
2026-05-12 22:39:25 +0300

    Update author and section details in Heap.java
    Updated author information and section details in comments.

--- src/Heap.java
+++ src/Heap.java
@@ -1,8 +1,8 @@
 //
 // Title: Heap Class
-// Author: [Your Name/Surname]
-// ID: [Your ID]
-// Section: [Your Section]
+// Author: [Elçin Karagül-Kayra Arı]
+// ID: [10885319050-10001507]
+// Section: [04]
 // Assignment: 4
 // Description: This class implements a min-heap structure for Course objects.
 //              Courses are ordered primarily according to their z values.
